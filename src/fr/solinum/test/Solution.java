3c093cf493b941f5b33e9d028300fb4ac5fc9a15
ALI <ali.belhadj@niji.fr>
2018-04-03 14:19:40 +0200

    solution

---
+++ src/fr/solinum/test/Solution.java
@@ -0,0 +1,46 @@
+package fr.solinum.test;
+
+import java.util.Scanner;
+
+	import java.util.*;
+	import java.io.*;
+	import java.math.*;
+
+	/**
+	 * Auto-generated code below aims at helping you parse
+	 * the standard input according to the problem statement.
+	 **/
+	class Solution {
+
+	    public static void main(String args[]) {
+	        Scanner in = new Scanner(System.in);
+	        int n = in.nextInt(); // the number of temperatures to analyse
+	        int plusProche=0;//va retourner 0 si n=0
+	        if(n>0) {
+	        	int[] result=new int[n];
+		        for (int i = 0; i < n; i++) {
+		            int t = in.nextInt(); // a temperature expressed as an integer ranging from -273 to 5526
+		            result[i]=t;//on va les mettres dans un tableau dabord
+		        }
+		        plusProche=Math.abs(result[0]);
+		        for(int i=1;i<n;i++) {
+		        	if(result[i]<0) {
+		        		if(Math.abs(result[i])<plusProche) {
+		        			plusProche=Math.abs(result[i]);
+		        		}
+		        	}else {
+		        		if(result[i]<plusProche) {
+		        			plusProche=result[i];
+		        		}
+		        	}
+		        }	
+	        }
+	        
+	        System.out.println(plusProche);
+	        		
+	        // Write an action using System.out.println()
+	        // To debug: System.err.println("Debug messages...");
+
+	        System.out.println("result");
+	    }
+}
